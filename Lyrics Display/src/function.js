ee6debd655f60d89df3108b33ebd39431dc84aa6
Anyl Feboe Uriarte <uriarteanylfeboe@gmail.com>
2025-06-09 22:51:19 +0800

    patched up the function and added animation

--- Lyrics Display/src/function.js
+++ Lyrics Display/src/function.js
@@ -1,24 +1,50 @@
 import { lyrics } from "./lyricsObs.js";
-import { timing } from "./lyricsObs.js";
+// import { timings } from "./lyricsObs.js";
 
     
     const lyricsBox = document.getElementById("lyricsBox");
+    const icarus = document.getElementById("icarus");
+    let hasStartedFrom215 = false;
+    let lyricsStarted = false;
     let textDuration = 0;
 
     function displayOfLyrics() {
 
-        lyrics.forEach((lyric, index) => {
+        lyrics.forEach((lyric) => {
 
-        setTimeout(() => {
+            setTimeout(() => {
 
             lyricsBox.textContent += lyric.text + " ";
 
-        }, textDuration);
+            }, textDuration);
         
-        textDuration += lyric.textDelay + timing[index];
-
-     });
+               textDuration += lyric.textDelay;
+        });
     
     }
 
-displayOfLyrics();
+    btn.addEventListener("click", function(){
+
+        const audio = document.getElementById('audio');
+        
+        
+        if (audio.paused) {
+            
+             if (!hasStartedFrom215) {
+                audio.currentTime = 216;
+                hasStartedFrom215 = true;
+            }
+
+                icarus.classList.add("animate");
+            
+                audio.play();
+            
+
+            if (!lyricsStarted) {
+                lyricsStarted = true;
+                displayOfLyrics();
+            }
+
+         }
+
+    });
