61dd670e17b5a93455a9675aa95ce5c609d3370b
Anyl Feboe Uriarte <uriarteanylfeboe@gmail.com>
2025-07-02 12:46:22 +0800

    initial operation

--- Calculator/script.js
+++ Calculator/script.js
@@ -18,3 +18,26 @@ function touchAnimate(event) {
     { once: true }
   );
 }
+
+function calc(a, b, answer, operation) {
+  let input = document.getElementById("input");
+  let output = document.getElementById("output");
+
+  switch (operation) {
+    case "+":
+      a += b;
+      break;
+    case "-":
+      a -= b;
+      break;
+    case "×":
+      a *= b;
+      break;
+    case "÷":
+      a /= b;
+      break;
+    default:
+      break;
+  }
+  return answer(operation);
+}
