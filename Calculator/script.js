8f45a24a50a66df8fb77d985f1c96f8e77050804
Anyl Feboe Uriarte <uriarteanylfeboe@gmail.com>
2025-07-02 11:58:12 +0800

    patched the button animation

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,4 +1,20 @@
+document.querySelectorAll(".input-buttons").forEach((btn) => {
+  btn.addEventListener("click", touchAnimate);
+  btn.addEventListener("touchStart", touchAnimate);
+});
 
-function name(params) {
-    
+function touchAnimate(event) {
+  const elem = event.currentTarget;
+
+  elem.style.animation = "none";
+  elem.offsetHeight;
+  elem.style.animation = "buttonAnimate 0.5s ease";
+
+  elem.addEventListener(
+    "animationEnd",
+    () => {
+      elem.style.animation = "";
+    },
+    { once: true }
+  );
 }
